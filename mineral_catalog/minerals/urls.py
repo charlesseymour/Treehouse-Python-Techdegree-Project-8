25c00507ad10668b983f594b7bdb838d6040aafe
charlesseymour <charlesseymour@yahoo.com>
2017-11-06 10:15:27 -0600

    Update urls.py

--- mineral_catalog/minerals/urls.py
+++ mineral_catalog/minerals/urls.py
@@ -1,8 +1,10 @@
-#minerals/urls.py
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     url(r'^$', views.mineral_list, name='list'),
-    url(r'(?P<pk>\d+)/$', views.mineral_detail, name='detail')
+    url(r'(?P<letter>[A-Z])/$', views.mineral_list, name='list'),
+    url(r'(?P<pk>\d+)/$', views.mineral_detail, name='detail'),
+    url(r'^group/(?P<group_name>[\w\ ]+)/$', views.mineral_group_list, name='group_list'),
+    url(r'search/$', views.search, name='search')
 ]
