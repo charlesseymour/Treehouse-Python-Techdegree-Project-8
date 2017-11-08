82e0b3c3fb11f388ccc60845db3b3d1cb75fab1b
charlesseymour <charlesseymour@yahoo.com>
2017-11-08 10:35:54 -0600

    Update tests.py

--- mineral_catalog/minerals/tests.py
+++ mineral_catalog/minerals/tests.py
@@ -17,26 +17,29 @@ class MineralViewsTests(TestCase):
         self.mineral1 = Mineral.objects.create(
             name="Kryptonite",
             image_caption="The weakness of Superman",
-            category="Fictional"
+            category="Radioactive",
+            group="Fictional"
         )
         self.mineral2 = Mineral.objects.create(
             name="Dilithium",
             image_caption="The favorite mineral of Scotty the Engineer",
-            category="Fictional"
+            category="Crystal",
+            group="Fictional"
         )
         self.mineral3 = Mineral.objects.create(
             name="Kainite",
             image_caption="Kainite",
-            category="Sulfate"
-        )    
+            category="Sulfate",
+            group="Sulfates"
+        )
+
     def test_mineral_list_view(self):
         resp = self.client.get(reverse('minerals:list',
                                        kwargs={'letter': 'K'}))
         self.assertEqual(resp.status_code, 200)
-        self.assertEquals(self.mineral1.name,
-                          resp.context['minerals'[0]['name']])
-        self.assertEquals(self.mineral3.name,
-                          resp.context['minerals'[1]['name']])
+        self.assertIn(self.mineral1, resp.context['minerals'])
+        self.assertIn(self.mineral3, resp.context['minerals'])
+        self.assertNotIn(self.mineral2, resp.context['minerals'])
         self.assertTemplateUsed(resp, 'minerals/list.html')
         self.assertContains(resp, self.mineral1.name)
         self.assertContains(resp, self.mineral3.name)
@@ -52,26 +55,27 @@ class MineralViewsTests(TestCase):
         self.assertContains(resp, self.mineral2.image_caption)
         self.assertContains(resp, "Category")
         self.assertContains(resp, self.mineral2.category)
-        
+
     def test_group_list_view(self):
         resp = self.client.get(reverse('minerals:group_list',
-                                       kwargs={'group': 'Fictional'}))
+                                       kwargs={'group_name': 'Fictional'}))
         self.assertEqual(resp.status_code, 200)
-        self.assertContains(self.mineral1, resp.context['minerals'])
-        self.assertContains(self.mineral2, resp.context['minerals'])
-        self.assertNotContains(self.mineral3, resp.context['minerals'])
+        self.assertEqual(len(resp.context['minerals']), 2)
+        self.assertIn(self.mineral1, resp.context['minerals'])
+        self.assertIn(self.mineral2, resp.context['minerals'])
+        self.assertNotIn(self.mineral3, resp.context['minerals'])
         self.assertTemplateUsed(resp, 'minerals/list.html')
-        self.assertContains(resp, self.mineral1.name)
-        self.assertContains(resp, self.mineral2.name)
-        self.assertNotContains(resp, self.mineral3.name)
-        
+        self.assertContains(resp, self.mineral1.name + '</a>')
+        self.assertContains(resp, self.mineral2.name + '</a>')
+        self.assertNotContains(resp, self.mineral3.name + '</a>')
+
     def test_search_view(self):
-        resp = self.client.get(reverse('minerals:search',
-                                       kwargs={'q': 'kainite'}))
+        resp = self.client.get(reverse('minerals:search'),
+                               {'q': 'kainite'})
         self.assertEqual(resp.status_code, 200)
-        self.assertContains(self.mineral3, resp.context['minerals'])
-        self.assertNotContains(self.mineral1, resp.context['minerals'])
-        self.assertNotContains(self.mineral2, resp.context['minerals'])
+        self.assertIn(self.mineral3, resp.context['minerals'])
+        self.assertNotIn(self.mineral1, resp.context['minerals'])
+        self.assertNotIn(self.mineral2, resp.context['minerals'])
         self.assertTemplateUsed(resp, 'minerals/list.html')
         self.assertContains(resp, self.mineral3.name)
         self.assertNotContains(resp, self.mineral1.name)
